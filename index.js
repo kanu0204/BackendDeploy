cea36882d468dd712c398f88bd7baa0629fe8726
Rishabh pal <palrishabh42@gmail.com>
2024-07-18 18:14:15 +0530

    Data Added for check json

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const express = require('express');
 const server = express();
 var port = 5000;
 
+const data = {
+	
+		"color": "red",
+		"value": "#f00"
+}
+
 server.get('/', (req, res) => {
     res.send(" hello my dera")
 });
 server.get('/gmail', (req, res) => {
     res.send('<h1> Hey hello</h1>')
+});
+server.get('/data', (req, res) => {
+    res.json(data)
 })
 
 server.listen(process.env.PORT, () => {
